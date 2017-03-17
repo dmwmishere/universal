18633527f05ed3156e446233e0733ac0cfe96c65
dmwmishere <dmwm88@gmail.com>
2017-03-17 22:32:04 +0300

    module split performed!

--- src/main/java/org/dmwm/universal/core/stats/StatsHolder.java
+++
@@ -1,8 +0,0 @@
-package org.dmwm.universal.core.stats;
-
-public interface StatsHolder {
-	public void putStat(String name);
-	public Long startOperation(String id);
-	public Long stopOperation(String id);
-//	public Map<String, Long> getAllStats();
-}
